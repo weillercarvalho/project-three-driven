1efb19f1a8db536bd6ee51a1005a415e8d3eb388
Weiller CArvalho <engweiller@hotmail.com>
2022-06-11 20:19:54 -0300

    status:finishingtheconditionalsofthejavascriptpart

--- script.js
+++ script.js
@@ -1,4 +1,57 @@
-function border() {
-    const a = document.querySelector(".subimages");
-    a.classList.add("clique")
+let a;
+let b;
+let c;
+let d;
+let e;
+let f;
+let g;
+
+function foods(elemento) {
+    
+    d = elemento.innerHTML;
+
+    a = document.querySelector(`.food .clique`);
+    if (a !== null) {
+        a.classList.remove("clique");
+        
+    } 
+    elemento.classList.add("clique");
+    verified();
+}
+function drinks(elemento) {
+
+    e = elemento.innerHTML;
+
+    b = document.querySelector(`.drink .clique`);
+    if (b !== null) {
+        b.classList.remove("clique");
+        
+    } 
+    elemento.classList.add("clique");
+    verified();
+}
+function desserts(elemento) {
+
+    f = elemento.innerHTML;
+
+    c = document.querySelector(`.dessert .clique`);
+    if (c !== null) {
+        c.classList.remove("clique");
+        
+    } 
+    elemento.classList.add("clique");
+    verified();
+}
+function verified() {
+    if (d && e && f){
+        g = document.querySelector(`.end-one`);
+        g.classList.add(`final-button`);
+        g.innerHTML = `Fechar pedido`;
+    } else {
+        g.classList.remove(`final-button`);
+        g.innerHTML = `Selecione os 3 itens para fechar o pedido`
+    }
+}
+function sendmessage(){
+
 }
